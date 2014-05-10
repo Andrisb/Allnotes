5eb39e220dfd3dfa6e3f13fe860d01a6c5bc9959
Andrisb <andrisb@outlook.com>
2014-05-10 19:30:56 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,99 @@
 <?php
 
 ?>
+<html>
+<head>
+<style type="text/css">
+	td {
+		font-family: arial;
+		font-size: 10px;
+	}
+	
+</style>
+</head>
+<body>
+<table style="width: 400px;">
+<tr>
+	<td colspan="7">
+		GANT chart
+	</td>
+</tr>
+<tr>
+	<td style="width: 150px; vertical-align: bottom;">description</td>
+	<td style="width: 100px; vertical-align: bottom;">begintime</td>
+	<td style="width: 50px; vertical-align: bottom;">iduration</td>
+	<td style="width: 200px; vertical-align: bottom;">
+		<table style="width: 200px;">
+		<tr>
+			<td colspan="2" style="text-align: center; width:120px;">GANT Indicator</td>
+		</tr>
+		<tr>
+			<td style="text-align: left; width: 100px;">9AM</td>
+			<td style="text-align: right;width: 100px;">5PM</td>
+		</tr>
+		</table>
+	</td>
+</tr>
+
+<tr>
+	<td>james bond</td>
+	<td>10:30</td>
+	<td>90</td>
+	
+	<td style="border: solid 1px grey;">
+	<img src="images/shim.gif" height="15px;" width="37.5px;"><img src="images/busy.gif" height="15px;" width="12.5px;">
+	</td>
+</tr>
+
+<tr>
+	<td>sally may</td>
+	<td>11:30</td>
+	<td>120</td>
+	
+	<td style="border: solid 1px grey;">
+	<img src="images/shim.gif" height="15px;" width="62.5px;"><img src="images/bluebar.gif" height="15px;" width="2px;"><img src="images/blue-line.gif" height="15px;" width="46px;"><img src="images/bluebar.gif" height="15px;" width="2px;">
+	</td>
+</tr>
+
+<tr>
+	<td>jerry jones</td>
+	<td>11:30</td>
+	<td>60</td>
+	
+	<td style="border: solid 1px grey;">
+	<img src="images/shim.gif" height="15px;" width="62.5px;"><img src="images/busy.gif" height="15px;" width="25px;">
+	</td>
+</tr>
+
+<tr>
+	<td>Mike Moxie</td>
+	<td>12:30</td>
+	<td>30</td>
+	
+	<td style="border: solid 1px grey;">
+	<img src="images/shim.gif" height="15px;" width="75px;"><img src="images/busy.gif" height="15px;" width="12.5px;">
+	</td>
+</tr>
+
+<tr>
+	<td>Carl Cluff</td>
+	<td>13:00</td>
+	<td>60</td>
+	
+	<td style="border: solid 1px grey;">
+	<img src="images/shim.gif" height="15px;" width="75px;"><img src="images/busy.gif" height="15px;" width="25px;">
+	</td>
+</tr>
+
+<tr>
+	<td>Mad Max</td>
+	<td>14:00</td>
+	<td>30</td>
+	
+	<td style="border: solid 1px grey;">
+	<img src="images/shim.gif" height="15px;" width="125px;"><img src="images/busy.gif" height="15px;" width="12.5px;">
+	</td>
+</tr>
+</table>
+</body>
+</html>
